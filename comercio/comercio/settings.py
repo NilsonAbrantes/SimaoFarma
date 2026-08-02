21d23a57d345b438b5253711cab650399572f411
NilsonAbrantes <lucasnilson624@gmail.com>
2026-08-02 11:55:51 -0300

    manifest

--- comercio/comercio/settings.py
+++ comercio/comercio/settings.py
@@ -186,7 +186,7 @@ STORAGES = {
     },
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
