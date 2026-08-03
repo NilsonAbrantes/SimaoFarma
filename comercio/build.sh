3a8bbc82a5288b2e7732437bcf9b840825aaaedc
NilsonAbrantes <lucasnilson624@gmail.com>
2026-08-03 09:35:11 -0300

    update

--- comercio/build.sh
+++ comercio/build.sh
@@ -2,9 +2,9 @@
 echo "Building project..."
 python -m pip install -r requirements.txt --break-system-packages
 
-echo "Make migrations..."
-python manage.py makemigrations
-python manage.py migrate
+# echo "Make migrations..."
+# python manage.py makemigrations
+# python manage.py migrate
 
 echo "Collect static..."
 python manage.py collectstatic --noinput
