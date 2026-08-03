93099096993ebc4864eebc872587c6a629a56c0d
NilsonAbrantes <lucasnilson624@gmail.com>
2026-08-03 10:13:42 -0300

    remove move

--- comercio/build.sh
+++ comercio/build.sh
@@ -4,6 +4,3 @@ python -m pip install -r requirements.txt --break-system-packages
 
 echo "Collect static..."
 python manage.py collectstatic --noinput
-
-echo "Movendo a pasta staticfiles para a raiz..."
-mv comercio/staticfiles .
